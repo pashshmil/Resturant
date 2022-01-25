835e99dd4a6f4c8c27dc651648d040e6608267da
Pavel Shmilevich <pavels@moveo.co.il>
2022-01-25 09:32:54 +0200

    search frame

--- src/Epicure/components/Shared/SearchInput/SearchInput.tsx
+++ src/Epicure/components/Shared/SearchInput/SearchInput.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './SearchInput.scss';
 // import IconButton from "@material-ui/core/IconButton";
 import InputAdornment from "@material-ui/core/InputAdornment";
@@ -6,13 +6,17 @@ import SearchIcon from "@material-ui/icons/Search";
 const search = require("../../../../assets/images/icons/search/search-icon.png");
 
 const SearchInput:React.FC<{title:string}>=(props)=> {
-  console.log("title ",props.title);
-  
+  // console.log("title ",props.title);
+  // const [title1,setTitle]=useState();
+  // setTitle(props.title);
   return (
-    <section className='header-box'>
-      <input className='header-input' type="text"/>
+    <section>
+      {props.title==='header' && <input className="header-input" placeholder='chefs,restuarants' type="text"/>}
+      {props.title==='home' && <input className="home-input" placeholder='chefs,restuarants' type="text"/>}
     </section>
   );
 }
 
 export default SearchInput
+
+
