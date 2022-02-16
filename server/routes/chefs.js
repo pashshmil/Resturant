b9605495541183f0365f46da585ae0d73b995ce0
Pavel Shmilevich <pavels@moveo.co.il>
2022-02-16 17:22:48 +0200

    last one

--- server/routes/chefs.js
+++ server/routes/chefs.js
@@ -1,4 +1,6 @@
 const express= require('express');
+const controller=require('../controllers/chefsController');
+
 const router=express.Router();
   router.get('/',controller.get);
 router.get('/:id',controller.getById);
